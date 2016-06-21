bd98cbea7a9a3c27714bd19c6a872192cbb4f683
KJ <xiankai@gmail.com>
2016-06-21 19:26:57 +0800

    Attempt to debug why bot disconnects

--- bot.js
+++ bot.js
@@ -13,4 +13,24 @@ bot.loginWithToken(token, () => {
 	console.log('logged in!');
 });
 
+bot.on('disconnected', () => {
+	let date = new Date();
+	console.log('disconnected for whatever reason at ' + date.toString());
+	bot.loginWithToken(token, () => {
+		console.log('logged in again!');
+	});	
+});
+
+bot.on('warn', error => {
+	let date = new Date();
+	console.log('warning encountered at ' + date.toString());
+	console.log('warning object is ', error);
+});
+
+bot.on('error', error => {
+	let date = new Date();
+	console.log('error encountered at ' + date.toString());
+	console.log('error object is ', error);
+});
+
 module.exports = bot;
